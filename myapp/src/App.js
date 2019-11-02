90d7816423cf0468e7d0f21812becf2feae0ff21
safaa <khadim.safaa@gmail.com>
2019-11-02 19:34:22 +0100

    add the header and footer and general code architecture

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,25 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route} from "react-router-dom";
+
+import Header from "./components/header.component"
+import Footer from "./components/footer.component"
+import MoviesList from "./components/movies-list.component";
+import Details from "./components/details.component";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="container">
+      <Header />
+      <Footer />
+      <br/>
+      <Route path="/list" exact component={MoviesList} />
+      <Route path="/details/:id" component={Details} />
+      </div>
+    </Router>
   );
 }
 
---
+++ myapp/src/components/movies-list.component.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+
+export default class MoviesList extends Component {
+
+  render() {
+    return (
+      <p>list</p>
+    );
+  }
+}
