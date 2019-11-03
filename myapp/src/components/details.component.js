9cb27477df74716a30ed623d4b8d35d6c7f3e883
safaa <khadim.safaa@gmail.com>
2019-11-03 16:18:19 +0100

    now i can pass the date of selected item to another component

--- myapp/src/components/details.component.js
+++ myapp/src/components/details.component.js
@@ -1,11 +1,107 @@
-import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import clsx from 'clsx';
+import Card from '@material-ui/core/Card';
+import CardHeader from '@material-ui/core/CardHeader';
+import CardMedia from '@material-ui/core/CardMedia';
+import CardContent from '@material-ui/core/CardContent';
+import CardActions from '@material-ui/core/CardActions';
+import Collapse from '@material-ui/core/Collapse';
+import Avatar from '@material-ui/core/Avatar';
+import IconButton from '@material-ui/core/IconButton';
+import Typography from '@material-ui/core/Typography';
+import { red } from '@material-ui/core/colors';
+import FavoriteIcon from '@material-ui/icons/Favorite';
+import ShareIcon from '@material-ui/icons/Share';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import MoreVertIcon from '@material-ui/icons/MoreVert';
+import InfoIcon from '@material-ui/icons/Info';
 
-export default class Details extends Component {
+const useStyles = makeStyles(theme => ({
+  card: {
+    maxWidth: 345,
+  },
+  media: {
+    height: 0,
+    paddingTop: '56.25%', // 16:9
+  },
+  expand: {
+    transform: 'rotate(0deg)',
+    marginLeft: 'auto',
+    transition: theme.transitions.create('transform', {
+      duration: theme.transitions.duration.shortest,
+    }),
+  },
+  expandOpen: {
+    transform: 'rotate(180deg)',
+  },
+  avatar: {
+    backgroundColor: red[500],
+  },
+}));
 
-  render() {
-    return (
-      <p>details</p>
-    );
-  }
+export default function Details(props) {
+  const classes = useStyles();
+  const [expanded, setExpanded] = React.useState(false);
+
+  const handleExpandClick = () => {
+    setExpanded(!expanded);
+  };
+
+  return (
+    <Card className={classes.card}>
+      <CardHeader
+
+        action={
+          <IconButton aria-label="settings">
+            <MoreVertIcon />
+          </IconButton>
+        }
+        title={props.name}
+        subheader={props.type}
+      />
+      <CardMedia
+        className={classes.media}
+        image={props.image}
+        title="Paella dish"
+      />
+      <CardContent>
+        <Typography variant="body2" color="textSecondary" component="p">
+        {props.score}
+        </Typography>
+      </CardContent>
+      <CardActions disableSpacing>
+        <IconButton aria-label="add to favorites">
+          <FavoriteIcon />
+        </IconButton>
+        <IconButton
+        onClick={props.showDetails}
+        aria-label="info">
+
+          <InfoIcon />
+        </IconButton>
+        <IconButton
+          className={clsx(classes.expand, {
+            [classes.expandOpen]: expanded,
+          })}
+          onClick={handleExpandClick}
+          aria-expanded={expanded}
+          aria-label="show more"
+        >
+          <ExpandMoreIcon />
+        </IconButton>
+      </CardActions>
+      <Collapse in={expanded} timeout="auto" unmountOnExit>
+        <CardContent>
+          <Typography paragraph>Summary:</Typography>
+
+          <Typography paragraph>
+          {props.summary}
+          </Typography>
+
+
+        </CardContent>
+      </Collapse>
+    </Card>
+  );
 }
--- myapp/src/components/movies-list.component.js
+++ myapp/src/components/movies-list.component.js
@@ -4,13 +4,16 @@ import axios from 'axios';
 import RecipeReviewCard from './card.js'
 import Grid from '@material-ui/core/Grid';
 import CustomizedGridList from './gridlist';
-
+import Details from './details.component';
 
 
 const Movies = props => (
    <RecipeReviewCard  name={props.movie.show.name} image={props.movie.show.image.medium} summary={props.movie.show.summary} type={props.movie.show.type} score={props.movie.score} showDetails={props.showDetails(props.movie)}></RecipeReviewCard>
 )
 
+const Onemovie = props =>(
+  <Details name ={props.movie.show.name}></Details>
+)
 
 export default class MoviesList extends Component {
 
@@ -18,7 +21,7 @@ export default class MoviesList extends Component {
   constructor(props) {
   super(props);
   this.showDetails = this.showDetails.bind(this);
-  this.state = {movies: [] , selectedmovie : []};
+  this.state = {movies: [] , selectedmovie : [] , detail: ''};
 
   }
 
@@ -40,8 +43,15 @@ export default class MoviesList extends Component {
   this.setState({
       selectedmovie: this.state.movies.filter(el => el== movie)
     });
+    this.setState({
+        detail: 'true'
+      });
   }
 
+  detail(){
+    console.log(this.state.selectedmovie[0]);
+    return <Onemovie movie={this.state.selectedmovie[0]} />
+  }
 
   //function 2 : movielist
   movielist() {
@@ -59,7 +69,14 @@ export default class MoviesList extends Component {
 
   //render
   render() {
-  return(
+    if(this.state.detail== 'true'){
+      return(
+      <div>
+          {this.detail()}
+      </div>
+    )
+    }
+    return(
     <CustomizedGridList list=  { this.movielist() }>
     </CustomizedGridList>
       );
