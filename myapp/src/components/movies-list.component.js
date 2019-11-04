8d591213d1aace7961027c7aab4cddc3fb42e291
safaa <khadim.safaa@gmail.com>
2019-11-04 04:51:07 +0100

    code refactoring : movies-list

--- myapp/src/components/movies-list.component.js
+++ myapp/src/components/movies-list.component.js
@@ -3,29 +3,29 @@ import { Link } from 'react-router-dom';
 import axios from 'axios';
 import RecipeReviewCard from './card/card.js'
 import Grid from '@material-ui/core/Grid';
-
 import CustomizedGridList from './gridlist/gridlist';
 import Details from './details/details.component';
 import Search from './search/search';
 
 
-
+//___________________________________________________________________________________________________
 const Movies = props => (
   <div>
-
    <RecipeReviewCard  name={props.movie.show.name} image={props.movie.show.image.medium} summary={props.movie.show.summary} type={props.movie.show.type} score={props.movie.score} showDetails={props.showDetails(props.movie)}></RecipeReviewCard>
 </div>
 )
 
 const MovieList = props => (
-
    <RecipeReviewCard  name={props.movie.show.name} image={props.movie.show.image.medium} summary={props.movie.show.summary} type={props.movie.show.type} score={props.movie.score} showDetails={props.showDetails(props.movie)}></RecipeReviewCard>
 )
 
 const Onemovie = props =>(
-
   <Details name ={props.movie.show.name} image ={props.movie.show.image.original} languange={props.movie.show.language} rating={props.movie.show.rating.average} officialSite={props.movie.show.officialSite} summary={props.movie.show.summary}></Details>
 )
+//___________________________________________________________________________________________________
+
+
+
 
 export default class MoviesList extends Component {
 
@@ -63,7 +63,7 @@ export default class MoviesList extends Component {
 
 
 
- //function 1 : showDetails
+ //function  : showDetails
   showDetails = movie =>() =>{
   this.setState({
       selectedmovie: this.state.movies.filter(el => el== movie)
@@ -76,7 +76,6 @@ export default class MoviesList extends Component {
 
 search(){
   console.log('seaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaarch');
-
   return <Search MovieList={this.showMovies} />
 }
 
@@ -84,25 +83,16 @@ search(){
 
 
   MovieList = id =>()=>{
-    console.log("aaaaaaaaaaaaaaaaaaaaaaa");
-    console.log(this);
-    //this.showMovies(id);
     return this.state.movies.map(currentmovie => {
       if(currentmovie.show.image == null){
         currentmovie.show.image={medium :"https://cidco-smartcity.niua.org/wp-content/uploads/2017/08/No-image-found.jpg"}
       }
-      console.log(currentmovie);
-        console.log(this.state.selectedmovie);
       return <MovieList movie={currentmovie} showMovies={this.showMovies} showDetails={this.showDetails} />;
-    }
-
-  )
-
-
+    })
   }
 
   detail(){
-    ////////////handle exceptions
+    ////////////---------------------handle exceptions-------------------------------
     //null original image
     console.log(this.state.selectedmovie[0]);
     if(this.state.selectedmovie[0].show.image == null){
@@ -120,18 +110,18 @@ search(){
     if(this.state.selectedmovie[0].show.summary == null){
       this.state.selectedmovie[0].show.summary="no information "
     }
-
+    ////////////--------------------fin-handle exceptions-------------------------------
     return <Onemovie movie={this.state.selectedmovie[0]} />
   }
 
   //function 2 : movielist
   movielist() {
+    ////////////---------------------handle exceptions-------------------------------
     return this.state.movies.map(currentmovie => {
       if(currentmovie.show.image == null){
         currentmovie.show.image={medium :"https://cidco-smartcity.niua.org/wp-content/uploads/2017/08/No-image-found.jpg"}
       }
-      console.log(currentmovie);
-        console.log(this.state.selectedmovie);
+      ////////////--------------------fin-handle exceptions-------------------------------
       return <Movies movie={currentmovie} showDetails={this.showDetails} key={currentmovie.show.id} showMovies={this.showMovies}/>;
     })
   }
@@ -140,16 +130,15 @@ search(){
 
   //render
   render() {
-
     if(this.state.detail== 'true'){
       return(
       <div>
           {this.search()}
+          
           {this.detail()}
       </div>
     )
     }
-
     return(
       <div>
     {this.search()}
